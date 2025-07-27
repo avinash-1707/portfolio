de5d1b288a6291656454326ccee14e274d9882cb
Avinash <avinashnarwariya5@gmail.com>
2025-07-27 22:53:50 +0530

    blog#1 tab data input

---
+++ src/config/Blogs.tsx
@@ -0,0 +1,11 @@
+import { Blog } from "@/types/Blog";
+
+export const Blogs: Blog[] = [
+  {
+    title: "Frontend Architecture Patterns",
+    description:
+      "In this blog we are going to discuss about different frontend architecture patterns and how they interact within",
+    date: "28 July 2025",
+    tags: ["architecture", "fullstack", "frontend", "mvp", "mvc"],
+  },
+];
---
+++ src/types/Blog.ts
@@ -0,0 +1,6 @@
+export interface Blog {
+  title: string;
+  description: string;
+  date: string;
+  tags: Array<string>;
+}
